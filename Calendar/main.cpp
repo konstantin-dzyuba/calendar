346b044713bf571f6796d8059d36eb479c54042a
Konstantin Dzyuba <konstantin.dzyuba.2003@gmail.com>
2024-12-20 19:07:55 +0300

    Катя

--- Calendar/main.cpp
+++ Calendar/main.cpp
@@ -7,39 +7,25 @@
 
 using namespace std;
 
-const array<string, 12> months = { "ﬂÕ¬¿–‹", "‘≈¬–¿À‹", "Ã¿–“", "¿œ–≈À‹", "Ã¿…", "»ﬁÕ‹", "»ﬁÀ‹", "¿¬√”—“", "—≈Õ“ﬂ¡–‹", "Œ “ﬂ¡–‹", "ÕŒﬂ¡–‹", "ƒ≈ ¿¡–‹" };
-const array<string, 7> days = { "œÕ", "¬“", "—–", "◊“", "œ“", "—¡", "¬—" };
+const array<string, 12> months = { "–Ø–ù–í–ê–†–¨", "–§–ï–í–†–ê–õ–¨", "–ú–ê–†–¢", "–ê–ü–†–ï–õ–¨", "–ú–ê–ô", "–ò–Æ–ù–¨", "–ò–Æ–õ–¨", "–ê–í–ì–£–°–¢", "–°–ï–ù–¢–Ø–ë–†–¨", "–û–ö–¢–Ø–ë–†–¨", "–ù–û–Ø–ë–†–¨", "–î–ï–ö–ê–ë–†–¨" };
+const array<string, 7> days = { "–ü–ù", "–í–¢", "–°–†", "–ß–¢", "–ü–¢", "–°–ë", "–í–°" };
 const array<int, 12> month_days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-const string message_welcome = "ƒÓ·Ó ÔÓÊ‡ÎÓ‚‡Ú¸";
-const string message_format = "¬‚Â‰ËÚÂ ‰‡ÚÛ ‚ ÙÓÏ‡ÚÂ ÃÃ √√√√";
-const string message_year = "√Ó‰ ‰ÓÎÊÂÌ ·˚Ú¸ ‚ ‰Ë‡Ô‡ÁÓÌÂ ÓÚ 1919 ‰Ó 2069";
-const string message_month = "ÃÂÒˇˆ ‰ÓÎÊÂÌ ·˚Ú¸ ‚ ‰Ë‡Ô‡ÁÓÌÂ ÓÚ 1 ‰Ó 12";
-const string message_exit = "¬‚Â‰ËÚÂ exit ‰Îˇ ‚˚ıÓ‰‡";
-const string message_bye = "ƒÓ Ò‚Ë‰‡ÌËˇ!";
+const string message_welcome = "–î–æ–±—Ä–æ –ø–æ–∂–∞–ª–æ–≤–∞—Ç—å";
+const string message_format = "–í–≤–µ–¥–∏—Ç–µ –¥–∞—Ç—É –≤ —Ñ–æ—Ä–º–∞—Ç–µ –ú–ú –ì–ì–ì–ì";
+const string message_year = "–ì–æ–¥ –¥–æ–ª–∂–µ–Ω –±—ã—Ç—å –≤ –¥–∏–∞–ø–∞–∑–æ–Ω–µ –æ—Ç 1919 –¥–æ 2069";
+const string message_month = "–ú–µ—Å—è—Ü –¥–æ–ª–∂–µ–Ω –±—ã—Ç—å –≤ –¥–∏–∞–ø–∞–∑–æ–Ω–µ –æ—Ç 1 –¥–æ 12";
+const string message_exit = "–í–≤–µ–¥–∏—Ç–µ exit –¥–ª—è –≤—ã—Ö–æ–¥–∞";
+const string message_bye = "–î–æ —Å–≤–∏–¥–∞–Ω–∏—è!";
 
 bool leap_year(int year) {
-	if (year % 400 == 0) {
-		return true;
-	} else {
-		if (year % 100 == 0) {
-			return false;
-		} else {
-			if (year % 4 == 0) {
-				return true;
-			}
-		}
-	}
-
-	return false;
+    return (year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
 }
 
 int first_day(int month, int year) {
 	int days = 2;
-
 	while (month-- > 0) {
 		days += month_days.at(month);
-
 		if (leap_year(year) && month == 1) {
 			days++;
 		}
@@ -58,11 +44,9 @@ int first_day(int month, int year) {
 
 void calendar(int month, int year) {
 	cout << months.at(month) << ' ' << year << endl;
-
 	for (int i = 0; i < days.size(); i++) {
 		cout << setw(WIDTH) << days.at(i);
 	}
-
 	cout << endl;
 
 	int first = first_day(month, year);
@@ -80,7 +64,6 @@ void calendar(int month, int year) {
 			cout << endl;
 		}
 	}
-
 	cout << endl;
 }
 
@@ -88,17 +71,12 @@ void test_first_day() {
 	array<int, 12> test_set_1919 = { 2, 5, 5, 1, 3, 6, 1, 4, 0, 2, 5, 0 };
 	array<int, 12> test_set_1920 = { 3, 6, 0, 3, 5, 1, 3, 6, 2, 4, 0, 2 };
 	array<int, 12> test_set_1921 = { 5, 1, 1, 4, 6, 2, 4, 0, 3, 5, 1, 3 };
-	array<int, 12> test_set_1922 = { 6, 2, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
-
-	array<array<int, 12>, 4> test_set = { test_set_1919, test_set_1920, test_set_1921, test_set_1922 };
-
+	array<array<int, 12>, 4> test_set = { test_set_1919, test_set_1920, test_set_1921 };
 	for (int i = 0; i < test_set.size(); i++) {
 		cout << i + 1919 << endl;
-
 		for (int j = 0; j < 12; j++) {
 			cout << (first_day(j, 1919 + i) == test_set.at(i).at(j)) << ' ' << months.at(j) << ' ' << first_day(j, 1919 + i) << ' ' << test_set.at(i).at(j) << endl;
 		}
-
 		cout << endl;
 	}
 }
@@ -106,11 +84,7 @@ void test_first_day() {
 int main() {
 	setlocale(0, "");
 
-	cout << message_welcome << endl;
-	cout << message_year << endl;
-	cout << message_month << endl;
-	cout << message_exit << endl;
-	cout << message_format << endl;
+	cout << message_welcome << endl << message_year << endl << message_month << endl << message_exit << endl << message_format << endl;
 
 	while (true) {
 		cout << endl;
@@ -126,14 +100,12 @@ int main() {
 			cout << message_format << endl;
 			continue;
 		}
-
 		int month = stoi(input.substr(0, 2));
 
 		if (month < 1 || month > 12) {
 			cout << message_month << endl;
 			continue;
 		}
-
 		int year = stoi(input.substr(3));
 
 		if (year < 1919 || year > 2069) {
